ab2bed1acadfcf2b925125a617e6a1da1a56895c
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-06 09:23:04 -0900

    Volume mount and ports

--- tools/fireball/modules/fireball/main.tf
+++ tools/fireball/modules/fireball/main.tf
@@ -1,29 +1,42 @@
+locals {
+  ports = concat(
+    [
+      4317,  // OLTP protobuf (write)
+      16685, // OLTP grpc API (read)
+      5778,  // HTTP sampling API
+      5779,  // gRPC sampling API
+      16686, // Web UI
+    ],
+    var.json ? [4318] : [],
+    var.zipkin ? [9411] : [],
+    var.legacy ? [
+      14250, // legacy protobuf (write)
+      14268, // legacy thrift (write)
+    ] : []
+  )
+}
+
 data "docker_image" "jaeger" {
   name = "jaegertracing/jaeger:2.0.0"
 }
 
-/*
-resource "docker_volume" "jaeger" {
-  name = "jaeger"
+resource "docker_volume" "config" {
+  name = "jaeger-config"
 }
-*/
 
 resource "docker_container" "jaeger" {
   image = data.docker_image.jaeger.id
-  name = "fireball"
-  /*
+  name  = "fireball"
+
   volumes {
-    volume_name = docker_volume.jaeger.name
-    container_path = ""
-  }
-  */
-  ports {
-    internal = 4317
-    external = 4317
+    volume_name    = docker_volume.config.name
+    container_path = "/etc/jaeger"
+    host_path      = "${path.cwd}/.fireball/etc/jaeger"
   }
 
-  ports {
-    internal = 16686
-    external = 16686
+  dynamic ports {
+    for_each = toset(local.ports)
+    internal = each.key
+    external = each.key
   }
 }
---
+++ tools/fireball/modules/fireball/variables.tf
@@ -0,0 +1,17 @@
+variable "json" {
+  description = "Whether or not to expose the OLTP JSON API on 4318"
+  type        = bool
+  default     = false
+}
+
+variable "zipkin" {
+  description = "Whether or not to expose the Zipkin API on 9411"
+  type        = bool
+  default     = false
+}
+
+variable "legacy" {
+  description = "Whether or not to expose legacy APIs"
+  type        = bool
+  default     = false
+}
