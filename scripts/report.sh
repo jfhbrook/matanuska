897aeb763bf120c4e79a0eb4d5525ee0592a72d2
Josh Holbrook <josh.holbrook@gmail.com>
2025-07-28 07:47:33 -0800

    Report with Tokei

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,54 +1,74 @@
 #!/usr/bin/env bash
 
-MATBAS_LC="$(cat ./*.ts \
-  ./ast/index.citree ./ast/util.ts \
-  ./bin/* \
-  ./bytecode/* \
-  ./commands/* \
-  ./examples/*.bas \
-  ./scripts/* \
-  ./value/* \
-  ./test/*.ts \
-  ./test/helpers/*.ts \
-  ./test/value/*.ts | wc -l)"
-CITREE_LC="$(cat ./packages/citree/src/*.ts \
-  ./packages/citree/src/templates/* | wc -l)"
-ENTRY_LC="$(cat ./packages/entrypoint/src/*.ts \
-  ./packages/entrypoint/modules/entrypoint/*.tf | wc -l)"
-FIREBALL_LC="$(cat ./packages/fireball/src/*.ts \
-  ./packages/fireball/modules/fireball/*.tf | wc -l)"
-GRABTHAR_LC="$(cat ./packages/grabthar/*.mjs | wc -l)"
-TESTGEN_LC="$(cat ./packages/test-generator/*.ts | wc -l)"
+function report {
+  tokei . -s lines "$@"
+}
 
-TOTAL_LC=$((MATBAS_LC + CITREE_LC + ENTRY_LC + FIREBALL_LC + GRABTHAR_LC + TESTGEN_LC))
+function named-report {
+  local path
+  local name
 
-echo '======== line counts ========'
-echo "project                    count"
-echo "--------                  ------"
-echo "main                    ${MATBAS_LC}"
-echo "packages/citree         ${CITREE_LC}"
-echo "packages/entrypoint     ${ENTRY_LC}"
-echo "packages/fireball       ${FIREBALL_LC}"
-echo "packages/grabthar       ${GRABTHAR_LC}"
-echo "packages/test-generator ${TESTGEN_LC}"
-echo "total                      ${TOTAL_LC}"
+  path="$(pwd)"
+  name="$(basename "${path}")"
 
-README_WC="$(wc -w < README.md)"
-ADR_WC="$(cat ./adrs/*.md | wc -w)"
-ADR_DRAFT_WC="$(cat ./adrs/draft/*.md | wc -w)"
-TOTAL_WC=$((README_WC + ADR_WC + ADR_DRAFT_WC))
+  echo "# Line Counts: ${name}"
+  report "$@"
+  echo
+}
 
-echo ''
-echo '=== word counts ==='
-echo "project       count"
-echo "--------     ------"
-echo "README     ${README_WC}"
-echo "adrs       ${ADR_WC}"
-echo "adrs/drafts${ADR_DRAFT_WC}"
-echo "total         ${TOTAL_WC}"
+function markdown-report {
+  README_WC="$(wc -w < README.md)"
+  ADR_WC="$(cat ./adrs/*.md | wc -w)"
+  ADR_DRAFT_WC="$(cat ./adrs/draft/*.md | wc -w)"
+  TOTAL_WC=$((README_WC + ADR_WC + ADR_DRAFT_WC))
 
-echo ''
-echo '=== artifact sizes ==='
-echo 'size	          file'
-echo '----	--------------'
-du -h ./dist/main.js
+  echo '# Word Counts'
+  echo '===================='
+  echo " Project      Count"
+  echo '===================='
+  echo "README     ${README_WC}"
+  echo "adrs       ${ADR_WC}"
+  echo "adrs/drafts${ADR_DRAFT_WC}"
+  echo '===================='
+  echo "Total         ${TOTAL_WC}"
+  echo '===================='
+}
+
+function artifact-report {
+  echo '# Artifact Sizes'
+  echo '======================='
+  echo ' Size	          File'
+  echo '======================='
+  du -h ./dist/main.js
+  echo '======================='
+}
+
+FULL=1
+ARGV=()
+
+while [[ $# -gt 0 ]]; do
+  case "${1}" in
+    --short)
+      FULL=''
+      shift
+      ;;
+    *)
+      ARGV+=("${1}")
+      ;;
+  esac
+done
+
+if [ -n "${FULL}" ]; then
+  named-report --exclude README.md --exclude adrs
+  markdown-report
+
+  for package in ./packages/*; do
+    (cd "${package}" && named-report)
+  done
+
+  echo ''
+  artifact-report
+else
+  report .
+fi
+
