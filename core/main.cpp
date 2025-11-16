7b4fe19ed20ec60ee7d5fd2b6451a363a19cee02
Josh Holbrook <josh.holbrook@gmail.com>
2025-11-16 13:10:44 -0900

    QJSEngine hello world works

--- core/main.cpp
+++ core/main.cpp
@@ -5,25 +5,6 @@
 #include <QJSValue>
 #include <QString>
 
-/*
-
-Uncaught Error: ponyyy
-    at foo (REPL1:1:24)
-
- * file:///Users/josh/code/jfhbrook/matanuska/test.js:6
-  throw new Error('ponyyy');
-        ^
-
-Error: ponyyy
-    at bar (file:///Users/josh/code/jfhbrook/matanuska/test.js:6:9)
-    at foo (file:///Users/josh/code/jfhbrook/matanuska/test.js:2:3)
-    at file:///Users/josh/code/jfhbrook/matanuska/test.js:9:1
-    at ModuleJob.run (node:internal/modules/esm/module_job:377:25)
-    at async onImport.tracePromise.__proto__
-(node:internal/modules/esm/loader:671:26) at async
-asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:101:5)
-*/
-
 void logResult(QJSValue result) {
   QDebug warn = qWarning();
   warn.noquote();
@@ -48,6 +29,8 @@ int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
 
   QJSEngine engine;
+
+  engine.installExtensions(QJSEngine::ConsoleExtension);
 
   QJSValue result = engine.evaluate(DIST);
 
