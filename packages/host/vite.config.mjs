8e5971079613118c217d3428b0472065f5387ec7
Josh Holbrook <josh.holbrook@gmail.com>
2025-11-23 20:14:14 -0900

    Build host with grabthar

---
+++ packages/host/vite.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from '@jfhbrook/grabthar/vite';
+
+export default defineConfig({
+  ssr: {
+    external: [
+      '@matanuska/output',
+      '@matanuska/path',
+    ],
+  },
+  /*
+  build: {
+    external: [
+      '@matanuska/output',
+      '@matanuska/path',
+    ],
+  },
+  optimizeDeps: {
+    exclude: [
+      '@matanuska/output',
+      '@matanuska/path',
+    ],
+  },
+  */
+});
